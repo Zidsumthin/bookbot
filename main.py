a61e9a55c50ec637d37f53d5a0b96125fa0190fd
hovey <hoveypeter15@gmail.com>
2025-07-18 13:50:07 -0400

    completed book path code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(filepath):
+    full_text = " "
+
+    with open(filepath) as f:
+        full_text = f.read()
+
+    return full_text
+
+def main():
+    frankenstein = "/home/hovey/workspace/github.com/hovey/bookbot/books/frankenstein.txt"
+
+    print(get_book_text(frankenstein))
+
+main()
